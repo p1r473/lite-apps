3088b7454053a0b6cd7a947f74988fc206c94a52
Chimbori <hello@chimbori.com>
2018-03-04 23:45:46 -0800

    Read options from gradle.properties, and disable certain operations based on the options defined there.

--- tools/src/test/java/com/chimbori/liteapps/BlockListValidator.java
+++ tools/src/test/java/com/chimbori/liteapps/BlockListValidator.java
@@ -1,24 +1,36 @@
 package com.chimbori.liteapps;
 
-import com.chimbori.liteapps.BlockListPackager;
-
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 
 public class BlockListValidator {
-  private static final boolean FETCH_REMOTE_FILES = false;
+  /**
+   * Whether running this test should download blocklist files from remote servers.
+   * Set to false to speed up processing by avoiding a network fetch.
+   */
+  private final boolean SHOULD_DOWNLOAD;
+
+  /**
+   * Whether running this test should re-package blocklists.
+   * Since blocklists don’t need to be updated every time a new Lite App is added, this
+   * can be safely set to true unless explicitly updating the blocklists.
+   */
+  private final boolean SHOULD_PACKAGE;
+
+  public BlockListValidator() {
+    SHOULD_DOWNLOAD = Boolean.parseBoolean(System.getProperty("blocklists.download.enabled"));
+    SHOULD_PACKAGE = Boolean.parseBoolean(System.getProperty("blocklists.packaging.enabled"));
+  }
 
   @Test
-  public void testAssembleAllBlockLists() {
-    try {
-      if (FETCH_REMOTE_FILES) {
-        BlockListPackager.downloadFromSources();
-      }
+  public void testAssembleAllBlockLists() throws IOException {
+    if (SHOULD_DOWNLOAD) {
+      BlockListPackager.downloadFromSources();
+    }
+
+    if (SHOULD_PACKAGE) {
       BlockListPackager.packageBlockLists();
-    } catch (IOException e) {
-      Assert.fail(e.getMessage());
     }
   }
 }
