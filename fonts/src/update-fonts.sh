88a98cef1b7a023fcd352cbd9ffa1c11d6029bbb
Chimbori <hello@chimbori.com>
2018-03-18 09:20:42 -0700

    Change font filenames to reflect actual user-visible fonts, and remove logic from client app to change font display names.

--- fonts/src/update-fonts.sh
+++ fonts/src/update-fonts.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
-FONTS_DIR=basic-fonts
-# Remove existing downloaded fonts (both zip and directory)
-rm -Rf "${FONTS_DIR}.zip" "${FONTS_DIR}"
-mkdir -p "${FONTS_DIR}"
+
 # Fetch all the font TTFs again, because fonts are constantly being updated by their authors.
 # This is the canonical list of fonts; there is no other list. This is intentional to keep things simple and consistent.
-wget --directory-prefix=${FONTS_DIR} \
-    https://github.com/google/fonts/raw/master/apache/opensans/OpenSans-Regular.ttf  \
-    https://github.com/google/fonts/raw/master/apache/roboto/Roboto-Regular.ttf \
-    https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Medium.ttf \
-    https://github.com/google/fonts/raw/master/ofl/alegreya/Alegreya-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/cabin/Cabin-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/firasanscondensed/FiraSansCondensed-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/hammersmithone/HammersmithOne-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/lora/Lora-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/montserrat/Montserrat-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/muli/Muli-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/overpass/Overpass-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/quattrocentosans/QuattrocentoSans-Italic.ttf \
-    https://github.com/google/fonts/raw/master/ofl/raleway/Raleway-Regular.ttf \
-    https://github.com/google/fonts/raw/master/ofl/sanchez/Sanchez-Regular.ttf
-# Change to the subdirectory before zipping, so that the zip directory entry does not include any subdirectory names.
-cd "${FONTS_DIR}"
-# Use the best compression (option "-9")
-zip -9 "../${FONTS_DIR}.zip" *
-# Remove temporary files, and only keep the zip file.
-cd ..
-rm -Rf "${FONTS_DIR}"
+wget -O "Alegreya.ttf"             https://github.com/google/fonts/raw/master/ofl/alegreya/Alegreya-Regular.ttf
+wget -O "Cabin.ttf"                https://github.com/google/fonts/raw/master/ofl/cabin/Cabin-Regular.ttf
+wget -O "Fira Sans Condensed.ttf"  https://github.com/google/fonts/raw/master/ofl/firasanscondensed/FiraSansCondensed-Regular.ttf
+wget -O "Hammersmith One.ttf"      https://github.com/google/fonts/raw/master/ofl/hammersmithone/HammersmithOne-Regular.ttf
+wget -O "Lora.ttf"                 https://github.com/google/fonts/raw/master/ofl/lora/Lora-Regular.ttf
+wget -O "Montserrat.ttf"           https://github.com/google/fonts/raw/master/ofl/montserrat/Montserrat-Regular.ttf
+wget -O "Muli.ttf"                 https://github.com/google/fonts/raw/master/ofl/muli/Muli-Regular.ttf
+wget -O "Open Sans.ttf"            https://github.com/google/fonts/raw/master/apache/opensans/OpenSans-Regular.ttf
+wget -O "Overpass.ttf"             https://github.com/google/fonts/raw/master/ofl/overpass/Overpass-Regular.ttf
+wget -O "Quattrocento Sans.ttf"    https://github.com/google/fonts/raw/master/ofl/quattrocentosans/QuattrocentoSans-Italic.ttf
+wget -O "Raleway.ttf"              https://github.com/google/fonts/raw/master/ofl/raleway/Raleway-Regular.ttf
+wget -O "Roboto Mono.ttf"          https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Medium.ttf
+wget -O "Roboto.ttf"               https://github.com/google/fonts/raw/master/apache/roboto/Roboto-Regular.ttf
+wget -O "Sanchez.ttf"              https://github.com/google/fonts/raw/master/ofl/sanchez/Sanchez-Regular.ttf
