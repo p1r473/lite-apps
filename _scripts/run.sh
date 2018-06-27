3cefcc493dae4954bd3535be08f97f0d2ad0dd9a
Chimbori <hello@chimbori.com>
2018-06-26 22:43:15 -0700

    Enable minification for production, enable incremental builds for local builds, and two separate config files for prod and dev.

--- _scripts/run.sh
+++ _scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Runs the Jekyll server for development.
 babel --presets react --extensions .jsx --watch _site/library/ --out-dir library/ & # Run in the background.
-bundle exec jekyll serve --watch --trace
+bundle exec jekyll serve --watch --trace --incremental --config _config.yml,_config_dev.yml
