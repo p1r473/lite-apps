68d7cdbb5e6607cd16a618c05f472301919a7857
Chimbori <hello@chimbori.com>
2019-07-23 11:30:48 -0700

    Remove Urban Dictionary Scriptlet, rename “Google Define” to “Define Selected Text”

--- docs/scriptlets/Urban Dictionary.js
+++
@@ -1,7 +0,0 @@
-var q = document.getSelection();
-if (!q) {
-  q = prompt('What to look up?', '');
-}
-if (q) {
-  window.location = 'https://www.urbandictionary.com/define.php?term=' + encodeURIComponent(q);
-}
