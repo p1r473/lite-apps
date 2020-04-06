32fc6b365f2cb180a72533ea1c4b1bf3aa94f540
Chimbori <hello@chimbori.com>
2020-04-06 16:40:22 -0700

    Convert to Kotlin: LiteAppPackager

---
+++ tools/src/main/java/com/chimbori/liteapps/LiteAppPackager.kt
@@ -0,0 +1,33 @@
+package com.chimbori.liteapps
+
+import com.chimbori.FilePaths.LITE_APPS_OUTPUT_DIR
+import com.chimbori.FilePaths.MANIFEST_JSON_FILE_NAME
+import com.chimbori.common.FileUtils
+import com.eclipsesource.json.Json
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
+
+/**
+ * Packages all Lite Apps into their corresponding .hermit packages.
+ * Does not validate each Lite App prior to packaging: it is assumed that this is run on a green
+ * build which has already passed all validation tests.
+ */
+internal object LiteAppPackager {
+  /**
+   * Packages a single manifest from a source directory & individual files into a zipped file and
+   * places it in the correct location.
+   */
+  @JvmStatic
+  fun packageManifest(liteAppDirectory: File): Boolean {
+    val manifestJsonFile = File(liteAppDirectory, MANIFEST_JSON_FILE_NAME)
+    val liteAppZippedFile = File(LITE_APPS_OUTPUT_DIR, "${liteAppDirectory.name}.hermit")
+    try {
+      Json.parse(FileReader(manifestJsonFile))
+    } catch (e: IOException) {
+      return false
+    }
+    FileUtils.zip(liteAppDirectory, liteAppZippedFile)
+    return true
+  }
+}
