6096c73dcfe9d3bf418be3b9a617983eb9f5c381
Chimbori <hello@chimbori.com>
2020-04-06 14:22:42 -0700

    Setup project for Kotlin in the :tools module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+plugins {
+  kotlin("jvm") version "1.3.71"
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-  extra["kotlin_version"] = "1.3.61"
-
   repositories {
-    jcenter()
-  }
-
-  dependencies {
-    classpath(kotlin("gradle-plugin", version = rootProject.extra["kotlin_version"] as String?))
-    // NOTE: Do not place your application dependencies here; they belong
-    // in the individual module build.gradle files
+    mavenCentral()
   }
 }
 
 allprojects {
   repositories {
-    jcenter()
+    mavenCentral()
   }
 }
 
@@ -26,4 +22,8 @@ tasks.withType<KotlinCompile> {
   kotlinOptions {
     jvmTarget = "1.8"
   }
+}
+
+repositories {
+  mavenCentral()
 }
